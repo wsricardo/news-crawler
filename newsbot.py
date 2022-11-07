662ace85fef011ba593f62aac69385b066ac7eba
Dimensão Alfa <dimensaoalfa.info@gmail.com>
2022-11-07 16:27:55 -0300

    Module bot to capture news.

--- newsbot.py
+++ newsbot.py
@@ -14,7 +14,9 @@ class NewsBot:
             'bbc': self.__trackNewsBBC
         }
 
-    def bnRequest(self, url=self.url ):
+    def bnRequest(self, url=None ):
+        if url == None:
+            url = self.url
 
         return requests.get(url).content.decode()
         
@@ -36,7 +38,7 @@ class NewsBot:
             return self.trackLinks()
 
      
-    def trackNewsHTMLFile(self, html_content_file, source=''):
+    def trackNewsHTMLFile(self, html_content_file, source=None):
         
         if source.lower() == 'g1':
             return self.__trackNewsG1(html_content_file)
@@ -66,17 +68,81 @@ class NewsBot:
         return dnews
 
 
-    def __trackNewsBBC(self):
+    def __trackNewsBBC(self, data):
+        bbc_list_news = []
+        url_bbc_base = 'https://www.bbc.com'
 
-        return None
+        bbc_soup = BeautifulSoup(data, 'html.parser')
+        bbc_sections = bbc_soup.find_all('section', class_='bbc-iinl4t')
 
-    def __trackNewsCNN(self):
+        for sections_news in bbc_sections:
+            for news in section_news.select( 'a' ):
+                if news['href'].find[ 'topic' ] != -1:
+                    bbc_list_news.extend( [
+                        {
+                            'title': news.text,
+                            'href': news['href']
+                        }
+                    ])
+                    
+                else:
+                    bbc_list_news.extend( [
+                        {
+                            'title': news.text,
+                            'href': url_bbc_base+news['href']
+                        }
+                    ])
+        return bbc_list_news
 
-        return None
+    def __trackNewsCNN(self, data):
+        cnn_list_news = []
 
-    def trackLinks(self):
+        cnn_soup = BeautifulSoup(data, 'html.parser')
+        cnn_nw_data = cnn_soup.find_all('section')
 
-        return []
+        aux = None
+
+        for news in cnn_nw_data:
+            aux = news.find('a')
+
+            try:
+                aux = aux.attrs
+                cnn_list_news.extend( [ 
+                    {
+                        'title': aux['title'],
+                        'href': aux['href']
+                    }
+                ])
+
+            except:
+                pass
+
+        return cnn_list_news
+
+    def trackLinks(self, url=None, data=None):
+        page_list_links = [] 
+        if url == None and data != None:
+            soup = BeautifulSoup(data, 'html.parser')
+            links = soup.find_all( 'a' )
+
+        elif url != None:
+            try:
+                data = requests.get( url ).content.decode()
+            except:
+                print('Error in url request.')
+        else:
+            print('Data or url miss.')
+            return None
+
+        for i in links:
+            page_list_links.extend( [
+                {
+                    'text': i.text,
+                    'href': i['href']
+                }
+            ])
+
+        return page_list_links
 
 if __name__ == "__main__":
     
@@ -87,3 +153,5 @@ if __name__ == "__main__":
     bot = NewsBot(data)
     a = bot.trackNewsHTMLFile(data, 'g1')
     print( a )
+    for i in a :
+        print(f'\n\n{i["title"]}\n{i["href"]}')
