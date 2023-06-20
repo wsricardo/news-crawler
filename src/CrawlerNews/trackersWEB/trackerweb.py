7cd79cf6572449117b351a109ab5db7ac32c1491
WSRicardo <wandeson.math@gmail.com>
2023-06-20 16:11:07 -0300

    Tracker folha de São Paulo.

---
+++ src/CrawlerNews/trackersWEB/trackerweb.py
@@ -0,0 +1,12 @@
+import scrapy
+import resquests
+import httpx
+
+
+def trackermain(url: str) -> list[str]:
+    """
+    This tracker code. It will to get crawlling links of web pages.
+    """
+    
+    return []
+    
